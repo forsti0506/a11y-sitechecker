52c1ab21e041fe53974600d9caed9b47b344b091
Martin Forstner <forsti@live.at>
2021-05-21 22:52:34 +0200

    fix: add Files for jest

---
+++ tests/test-helper-functions.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import { Config } from '../lib/models/config';
+import { setupConfig } from '../lib/utils/setup-config';
+
+export async function cleanUpAfterTest(config: Config): Promise<void> {
+    if (config.imagesPath) {
+        fs.rmdirSync(config.imagesPath, { recursive: true });
+    }
+    if (config.resultsPath) {
+        fs.rmdirSync(config.resultsPath, { recursive: true });
+    }
+}
+
+export async function initBeforeTest(): Promise<Config> {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 500000;
+    const optionValues = {
+        json: true,
+        config: './tests/config.json',
+    };
+    return setupConfig(optionValues);
+}
