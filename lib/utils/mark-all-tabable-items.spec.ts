acd5b3acf45ef4b61d075fc699c113ada1cec6e9
Martin Forstner <martin@forsti.eu>
2021-12-29 16:15:12 +0100

    chore: back to chore 4.1.0 (wait for ts 4.6); fix: https url for tests

--- lib/utils/mark-all-tabable-items.spec.ts
+++ lib/utils/mark-all-tabable-items.spec.ts
@@ -22,15 +22,13 @@ describe('markAllTabaleItems', () => {
 
         const browser = await puppeteer.launch(config.launchOptions);
         const pages = await browser.pages();
-        await pages[0].goto('https://www.forsti.eu');
+        await pages[0].goto('https://forsti.eu');
         jest.spyOn(pages[0], 'evaluate').mockImplementation((fn) => {
             return Promise.resolve(['test1', 'test2']);
         });
 
         try {
-            return expect(
-                markAllTabableItems(pages[0], 'https://www.forsti.eu', config, urlResult),
-            ).resolves.not.toThrow();
+            return expect(markAllTabableItems(pages[0], 'https://forsti.eu', config, urlResult)).resolves.not.toThrow();
         } catch (e) {
             fail(e);
         }
